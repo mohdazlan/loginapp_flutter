0a0c67a74c4b4b3daa445035667747510bc5cfd4
Azlan <mhdazlanabaziz@gmail.com>
2024-06-08 17:10:19 +0800

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+      body: _buildUI(context),
+    );
+  }
+
+  Widget _buildUI(BuildContext context) {
+    return Column(
+      children: [
+        _header(context),
+      ],
+    );
+  }
+
+  Widget _header(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.25,
+      color: const Color.fromRGBO(230, 253, 253, 1.0),
+    );
+  }
+}
